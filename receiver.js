7ce523f6bb999da77fb0367db3f7787ad3802bda
didgusdud <64472390+didgusdud@users.noreply.github.com>
2021-04-26 23:05:12 +0900

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -3,7 +3,7 @@ var client  = mqtt.connect('mqtt://52.79.164.177');
 var fs = require('fs');
  
 client.on('connect', function () {
-      client.subscribe('topic');
+      client.subscribe('topic1');
 });
  
 client.on('message', function (topic, message) {
