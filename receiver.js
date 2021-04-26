aa055fe1d9173e9eef7c0e4546f33607342e3a20
didgusdud <64472390+didgusdud@users.noreply.github.com>
2021-04-26 22:50:39 +0900

    Add files via upload

---
+++ receiver.js
@@ -0,0 +1,13 @@
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://52.79.164.177');
+var fs = require('fs');
+ 
+client.on('connect', function () {
+      client.subscribe('receiver sub cws');
+});
+ 
+client.on('message', function (topic, message) {
+      data = JSON.parse(message);
+      fs.writeFileSync(data.file, data.msg);
+      client.end();
+});
---
+++ sender.js
@@ -0,0 +1,22 @@
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://52.79.164.177');
+
+var fs = require('fs');
+
+file = 'peaches.txt';
+msg = fs.readFileSync(file);
+
+buf = {
+    "file": file,
+    "msg": msg,
+};
+
+client.on('connect', function () {
+    client.subscribe('sender sub cws');
+    client.publish('sender pub cws', JSON.stringify(buf));
+});
+ 
+client.on('message', function (topic, message) {
+      console.log(topic + ' : send');
+      client.end();
+});
